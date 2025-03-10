47e9855aa1308435b5c47e27c0343a5c31f0704a
W1ndys <109416673+W1ndys@users.noreply.github.com>
2025-03-10 21:02:24 +0800

    chore(backup): Rename backup script archive to use consistent naming

--- app/backup-scripts.sh
+++ app/backup-scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BACKUP_NAME="backup_scripts.tar.gz"
+BACKUP_NAME="backup-scripts.tar.gz"
 
 # 创建备份，排除 data 和 logs 目录，排除.tar.gz文件
 tar -czf "$BACKUP_NAME" --exclude="./data" --exclude="./logs" . --exclude='*.tar.gz'
