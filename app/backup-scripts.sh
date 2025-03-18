8dcc2ee62afa4268a75d40d56e7d11ebb4e059bf
W1ndys <w1ndys@outlook.com>
2025-03-18 16:42:05 +0800

    chore(script): 删除备份脚本

--- app/backup-scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BACKUP_NAME="backup-scripts.tar.gz"
-
-# 创建备份，排除 data 和 logs 目录，排除.tar.gz文件
-tar -czf "$BACKUP_NAME" --exclude="./data" --exclude="./logs" . --exclude='*.tar.gz'
-
-echo "备份已完成: $BACKUP_NAME" 
