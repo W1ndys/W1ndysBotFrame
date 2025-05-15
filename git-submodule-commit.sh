da369e9872c7d16f8d7007adef6e231f50324261
W1ndys <w1ndys@outlook.com>
2025-05-15 22:13:07 +0800

    chore(子模块): 添加询问是否添加所有文件的功能

--- git-submodule-commit.sh
+++ git-submodule-commit.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# 询问是否需要添加所有文件，默认回车添加所有文件
+read -p "是否需要添加所有文件？(y/n，默认y): " add_all
+
+if [ -z "$add_all" ] || [ "$add_all" = "y" ]; then
+    git add .
+else
+    echo "请手动添加需要提交的文件后继续"
+    exit 1
+fi
+
 # 提交更改，提交信息为"更新子模块"
 git commit -m "chore(子模块): 更新子模块"
 
