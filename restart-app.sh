d6f4fe1fa87f1f284df257ab01e04fb235be0030
W1ndys <w1ndys@outlook.com>
2025-05-15 13:50:38 +0800

    feat(重启脚本): 增强虚拟环境检查功能，优化启动信息输出。删除run-app.sh脚本，合并其功能至restart-app.sh。

--- restart-app.sh
+++ restart-app.sh
@@ -23,8 +23,14 @@ else
     echo "PID文件不存在，直接启动新进程"
 fi
 
-# 激活虚拟环境
-. "../venv/bin/activate"
+# 检查虚拟环境是否存在
+if [ -d "../venv/bin" ]; then
+    echo "虚拟环境存在，激活虚拟环境..."
+    # 激活虚拟环境
+    . "../venv/bin/activate"
+else
+    echo "虚拟环境不存在，使用系统Python环境..."
+fi
 
 # 启动Python程序并捕获错误
 nohup python main.py >app.log 2> >(tee -a app.log) &
@@ -32,7 +38,7 @@ nohup python main.py >app.log 2> >(tee -a app.log) &
 # 保存新的PID到文件
 echo $! >app.pid
 
-echo "Python程序已在虚拟环境中启动，新的PID保存在app/app.pid中"
+echo "Python程序已启动，新的PID保存在app/app.pid中"
 
 find /home/bot/app/scripts \( -name ".git" -o -name ".gitignore" -o -name ".gitmodules" -o -name ".gitattributes" \) -exec rm -rf {} +
 
